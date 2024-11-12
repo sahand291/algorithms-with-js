7067837510224f38932bf254e139a584e0ff6841
Sahand <sahandkarimkhani@gmail.com>
2024-11-12 12:27:09 +0330

    test: add tests to binary search

--- src/binary-search/binary-search.spec.ts
+++ src/binary-search/binary-search.spec.ts
@@ -2,8 +2,32 @@ import { describe, test, expect } from "vitest";
 
 import binarySearch from "./binary-search";
 
-test("first test", () => {
+describe("binary search tests", () => {
 	const a = 5;
+	const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-	expect(a).toEqual(5);
+	test("it should find middle number", () => {
+		const res = binarySearch(arr, 5);
+		expect(res).toEqual(5);
+	});
+
+	test("it should find first index", () => {
+		const res = binarySearch(arr, 0);
+		expect(res).toEqual(0);
+	});
+
+	test("it should find last index", () => {
+		const res = binarySearch(arr, 10);
+		expect(res).toEqual(10);
+	});
+
+	test("it should find first half", () => {
+		const res = binarySearch(arr, 2);
+		expect(res).toEqual(2);
+	});
+
+	test("it should find second half", () => {
+		const res = binarySearch(arr, 9);
+		expect(res).toEqual(9);
+	});
 });
