655c2dc2d6e70fd5c92e1cefbc8a128f7bbb8426
Sahand <sahandkarimkhani@gmail.com>
2024-11-12 12:53:16 +0330

    feat(algorithms): binary search algorithm
    conmplete binary search algorithm

--- src/binary-search/binary-search.ts
+++ src/binary-search/binary-search.ts
@@ -1,3 +1,24 @@
-export default function binarySearch() {
-	return [1, 2, 3];
+export default function binarySearch(
+	numbers: number[],
+	item: number,
+): number | null {
+	let low = 0;
+	let high = numbers.length - 1;
+
+	while (low <= high) {
+		const mid = Math.floor((low + high) / 2);
+		const guess = numbers[mid];
+
+		if (guess === item) {
+			return mid;
+		}
+
+		if (guess > item) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
+
+	return null;
 }
